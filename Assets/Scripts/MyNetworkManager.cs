4657a2f9fbfc1528fdf05cfb45b3022aa3c85c23
VladD <dmitriev.vs.751@mail.ru>
2020-12-01 17:36:49 +0300

    Added network manager

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        Debug.Log("I connected to the server!");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+        Debug.Log($"Number of players: {numPlayers}");
+    }
+}
