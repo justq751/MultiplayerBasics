d2bd050a623467e7385632e96b89bfdcb25024d3
VladD <dmitriev.vs.751@mail.ru>
2020-12-01 18:39:45 +0300

    Added Sync Values for color and player names

---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SyncVar]
+    [SerializeField]
+    private string displayName = "Missing name";
+
+    [SyncVar]
+    [SerializeField]
+    private Color displayColour = Color.black;
+
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+    [Server]
+    public void SetDisplayColour(Color newDisplayColour)
+    {
+        displayColour = newDisplayColour;
+    }
+}
